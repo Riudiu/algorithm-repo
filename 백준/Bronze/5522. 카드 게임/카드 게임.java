4a7a60d68786aa07fa12c18b020528d8d26f8bed
Jiwoo Yun <lacvert13@gmail.com>
2023-04-09 17:19:28 +0900

    Update 카드 게임.java

--- 백준/Bronze/5522. 카드 게임/카드 게임.java
+++ 백준/Bronze/5522. 카드 게임/카드 게임.java
@@ -3,12 +3,7 @@ import java.util.Scanner;
 public class Main {
 	
 	public static void main(String[] args) {
-		 
-		// Danny군은 카드 게임을 하고 있다. 이 카드 게임은 5회의 게임으로 진행되며, 그 총점으로 승부를 하는 게임이다.
-		// Danny군의 각 게임의 득점을 나타내는 정수가 주어졌을 때, Danny군의 총점을 구하는 프로그램을 작성하라.
-		
 		Scanner scan = new Scanner(System.in);   
-		
 		int sum = 0;      // 총점을 담는 변수 sum
 		
 		for (int i = 0; i < 5; i++) {    // 반복문을 통해 5회 실행   
