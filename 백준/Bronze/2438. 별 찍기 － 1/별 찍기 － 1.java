e78f9c39b808a7768803e0ce8d3a184a12473f9c
Jiwoo Yun <lacvert13@gmail.com>
2023-04-09 17:19:58 +0900

    Update 별 찍기 － 1.java

--- 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -3,10 +3,6 @@ import java.util.Scanner;
 public class Main {
 	
 	public static void main(String[] args) {
-		
-		// 별 찍기 1번
-		// 첫째 줄에는 별 1개, 둘째 줄에는 별 2개, N번째 줄에는 별 N개를 찍는 문제
-		
 		Scanner scan = new Scanner(System.in);
 		int num = scan.nextInt();
 		
