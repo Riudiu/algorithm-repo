d5df7322a5bd1a24ea809f42f46fe2ec51650974
Jiwoo Yun <lacvert13@gmail.com>
2023-04-11 01:58:37 +0900

    [Bronze I] Title: 달팽이는 올라가고 싶다, Time: 40 ms, Memory: 33376 KB -BaekjoonHub

--- 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.py
+++ 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.py
@@ -4,6 +4,6 @@ import math
 dayMeter, nightSlipMeter, destination = map(int, input().split())
 
 dayWalk = dayMeter - nightSlipMeter  # 낮과 밤 포함헤서 달팽이가 하루동안 올라가는 거리
-day = (destination / dayWalk) - (nightSlipMeter / dayWalk)
-    
+day = (destination - nightSlipMeter) / dayWalk
+
 print(math.ceil(day))
