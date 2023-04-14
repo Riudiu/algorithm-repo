e758e832255ec3e9f19b8c943c41586f87203191
Jiwoo Yun <lacvert13@gmail.com>
2023-04-14 21:21:36 +0900

    [Bronze II] Title: 수 정렬하기, Time: 88 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.py
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.py
@@ -1,27 +1,10 @@
-import sys
-
-input = sys.stdin.readline
-sys.setrecursionlimit(10**8)
-
-def quick_sort(array):
-    if len(array) <= 1:
-        return array
-    
-    pivot = array[0]
-    tail = array[1:]
-
-    left_side = [x for x in tail if x <= pivot]
-    right_side = [x for x in tail if x > pivot]
-    
-    return quick_sort(left_side) + [pivot] + quick_sort(right_side)
-
 length = int(input())
 N = [] 
 
 for i in range(length):
     N.append(int(input()))
 
-N = quick_sort(N)
+N.sort()
 
 for n in N:
     print(n)
