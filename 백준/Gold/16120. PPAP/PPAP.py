41f00e688faba7e70ab1ffeaf66e7f8c92c7c48b
Jiwoo Yun <lacvert13@gmail.com>
2023-04-20 23:07:19 +0900

    [Gold IV] Title: PPAP, Time: 348 ms, Memory: 38308 KB -BaekjoonHub

--- 백준/Gold/16120. PPAP/PPAP.py
+++ 백준/Gold/16120. PPAP/PPAP.py
@@ -6,14 +6,12 @@ stack = []
 ppap = ['P', 'P', 'A', 'P']
 
 for c in S:
-    if stack[-4:] == ppap:  
-        for _ in range(4):
-            stack.pop() 
-        stack.append('P')
     stack.append(c)
+    if stack[-4:] == ppap:  
+        for _ in range(3):
+            stack.pop() 
 
 if stack == ppap or stack == ['P']:
     print("PPAP")
 else: 
     print("NP")
-
