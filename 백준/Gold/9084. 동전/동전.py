62eb867841a5d1f77fea89a1b1141a1119711add
Jiwoo Yun <lacvert13@gmail.com>
2023-05-01 00:35:42 +0900

    [Gold V] Title: 동전, Time: 68 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Gold/9084. 동전/동전.py
@@ -0,0 +1,22 @@
+import sys
+input = sys.stdin.readline
+
+T = int(input())
+answer = []
+
+for _ in range(T):
+    N = int(input())
+    coins = list(map(int, input().split()))
+    M = int(input())
+
+    dp = [0] * (M + 1)
+    dp[0] = 1   # 0원을 만들기 위한 경우의 수는 1
+
+    for coin in coins:
+        for j in range(coin, M + 1):
+            dp[j] += dp[j-coin]  # DP 점화식
+    
+    answer.append(dp[M])
+
+for a in answer:
+    print(a)  
