913c94286e1fd5ed69812672f5cd8da99d3ba5c2
Jiwoo Yun <lacvert13@gmail.com>
2023-05-01 21:19:06 +0900

    [Gold III] Title: 행렬 곱셈 순서, Time: 684 ms, Memory: 118084 KB -BaekjoonHub

--- 백준/Gold/11049. 행렬 곱셈 순서/행렬 곱셈 순서.py
+++ 백준/Gold/11049. 행렬 곱셈 순서/행렬 곱셈 순서.py
@@ -2,9 +2,10 @@ import sys
 input = sys.stdin.readline
 
 N = int(input())
+
 d = list(map(int, input().split()))
 for _ in range(N - 1):
-    _, c = map(int, input().split())
+    r, c = map(int, input().split())
     d.append(c)
 
 dp = [[0 for _ in range(N + 1)] for _ in range(N + 1)]
@@ -12,11 +13,12 @@ for diff in range(1, N):  # diff - 1부터 N까지의 격차 / N이 4일 경우 
      for i in range(1, N - diff + 1):
         j = i + diff
         
-        if i == j: 
+        if i == j:  # 조건 1: 서로 같은 경우 -> 0, dp테이블에 이미 0으로 초기화 된 상태이므로 건너뛰기
             continue
 
-        dp[i][j] = float('inf')
-        for k in range(i, j):
+        # 조건 2: i보다 j가 클 경우
+        dp[i][j] = float('inf')  # 무한으로 먼저 세팅
+        for k in range(i, j):  # k의 범위 - i ≤ k ≤ j-1
             dp[i][j] = min(dp[i][j], dp[i][k] + dp[k + 1][j] + (d[i - 1] * d[k] * d[j]))
 
 print(dp[1][N])
