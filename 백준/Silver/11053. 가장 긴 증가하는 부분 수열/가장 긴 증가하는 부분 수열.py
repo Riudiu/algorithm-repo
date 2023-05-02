181da63cd2ef46265392fd6951c5b15c9004b048
Jiwoo Yun <lacvert13@gmail.com>
2023-05-02 17:13:40 +0900

    Update 가장 긴 증가하는 부분 수열.py

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.py
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.py
@@ -2,16 +2,12 @@ import sys
 input = sys.stdin.readline
 
 N = int(input())
-arr = [0]
-arr += list(map(int, input().split()))
+arr = list(map(int, input().split()))
 
-dp = [1] * (N + 1)
-for i in range(2, N + 1):
-    temp = 0
-    for j in range(1, i):
+dp = [1] * N
+for i in range(N):
+    for j in range(i): 
         if arr[i] > arr[j]:
-            if temp < dp[j]:
-                temp = dp[j]
-    dp[i] = temp + 1
+            dp[i] = max(dp[i], dp[j] + 1)
 
 print(max(dp))
