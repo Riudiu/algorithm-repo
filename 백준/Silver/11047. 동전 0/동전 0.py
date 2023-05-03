b4ac649dade8ba3daad4764f90cbd694847470e1
Jiwoo Yun <lacvert13@gmail.com>
2023-05-03 11:09:21 +0900

    [Silver IV] Title: 동전 0, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/11047. 동전 0/동전 0.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+N, K = map(int, input().split())
+arr = [int(input()) for _ in range(N)]
+cnt = 0
+
+for n in reversed(arr):
+    if K == 0: break
+    if K < n: continue
+    cnt += K // n
+    K %= n
+
+print(cnt)
