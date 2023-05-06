0bdd585d96d6b1f98e03e5e34096c602397471f6
Jiwoo Yun <lacvert13@gmail.com>
2023-05-06 21:40:42 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.py
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.py
@@ -1,9 +1,6 @@
 N = int(input())
 
-for i in range(1, 2 * N):
-    star = ""   
-    if (i > N):
-        star += " " * (i - N) + "*" * (4 * N - 2 * i - 1)
-    else:
-        star += " " * (N - i) + "*" * (2 * i - 1)
-    print(star)
+for i in range(1, N + 1):
+    print(" " * (N - i) + "*" * (2 * i - 1))
+for i in reversed(range(1, N)):
+    print(" " * (N - i) + "*" * (2 * i - 1))
