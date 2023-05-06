3b9fa55301247615dea1ecf4b74354c77db8e292
Jiwoo Yun <lacvert13@gmail.com>
2023-05-06 21:56:38 +0900

    [Bronze III] Title: 별 찍기 - 8, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/2445. 별 찍기 － 8/별 찍기 － 8.py
@@ -0,0 +1,6 @@
+N = int(input())
+
+for i in range(1, N + 1):
+    print("*" * i + " " * (2 * N - 2 * i) + "*" * i)
+for i in reversed(range(1, N)):
+    print("*" * i + " " * (2 * N - 2 * i) + "*" * i)
