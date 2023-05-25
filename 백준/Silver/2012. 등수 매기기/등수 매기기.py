00587a5cde7177aef9223d1b7277091a7b259764
Jiwoo Yun <lacvert13@gmail.com>
2023-05-25 23:44:33 +0900

    [Silver III] Title: 등수 매기기, Time: 464 ms, Memory: 52640 KB -BaekjoonHub

--- 백준/Silver/2012. 등수 매기기/등수 매기기.py
+++ 백준/Silver/2012. 등수 매기기/등수 매기기.py
@@ -7,12 +7,8 @@ for i in range(N):
     arr.append(int(input()))
 arr.sort()
 
-tuple_list = []
-for i in range(N):
-    tuple_list.append((arr[i], i + 1))
-
 ans = 0
-for a, b in tuple_list:
-    ans += abs(a - b)
+for i in range(N):
+    ans += abs((i + 1) - arr[i])
 
 print(ans)
