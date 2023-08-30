31989706da63eb769ed4b1e28494bda22c07e3e0
Jiwoo Yun <lacvert13@gmail.com>
2023-08-30 21:47:07 +0900

    [Silver III] Title: 모든 순열, Time: 216 ms, Memory: 17420 KB -BaekjoonHub

---
+++ 백준/Silver/10974. 모든 순열/모든 순열.js
@@ -0,0 +1,26 @@
+const input = require("fs").readFileSync("/dev/stdin").toString().trim();
+const N = Number(input);
+
+let visited = new Array(N + 1).fill(false);
+let selected = [];
+let result = "";
+
+function dfs(depth) {
+  if (depth == N) {
+    result += `${selected.join(" ")}\n`;
+    return;
+  }
+  for (let i = 1; i <= N; i++) {
+    if (visited[i]) continue;
+
+    visited[i] = true;
+    selected.push(i);
+    dfs(depth + 1);
+
+    selected.pop();
+    visited[i] = false;
+  }
+}
+
+dfs(0);
+console.log(result);
